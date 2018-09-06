4f66b4daa1c3b6a3b029272bdf86b3361f0f79e8
Ciaran M <cmorinan@gmail.com>
2018-09-06 17:39:36 +0100

    Built domain and functionality

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,27 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.average
+    p "There is an average of #{Car.all.size / @@all} cars per owner."
+  end
+
+  def cars
+    Car.all.select { |car| car.owner == self }
+  end
+
+  def mechanics
+    cars.map { |car| car.mechanic }
+  end
 
 end
